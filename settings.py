dff9d47bb187c059e9cabca25f076f5a43c0d24c
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-06-21 17:31:02 -0400

    remove pwd

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'MTg3MDEtaGFzZWVi',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
